eef070401d0fa3da8368a24d6141a14a27412c4b
j.brenes <j.brenes@nextworks.it>
2019-07-26 16:16:12 +0200

    fixed VnfConfigurationScript JPA bugs

--- NFV_MANO_LIBS_DESCRIPTORS/src/main/java/it/nextworks/nfvmano/libs/descriptors/nsd/VnfConfigurationScript.java
+++ NFV_MANO_LIBS_DESCRIPTORS/src/main/java/it/nextworks/nfvmano/libs/descriptors/nsd/VnfConfigurationScript.java
@@ -5,12 +5,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 @Entity
 public class VnfConfigurationScript {
@@ -26,8 +33,16 @@ public class VnfConfigurationScript {
 	@JsonIgnore
 	private VnfLCMScripts vnfLcmScripts;
 	
+	
+	@ElementCollection(fetch=FetchType.EAGER)
+	@Fetch(FetchMode.SELECT)
+	@Cascade(org.hibernate.annotations.CascadeType.ALL)
 	private Map<String, String> args = new HashMap<>();
 	
+	
+	@ElementCollection(fetch=FetchType.EAGER)
+	@Fetch(FetchMode.SELECT)
+	@Cascade(org.hibernate.annotations.CascadeType.ALL)
 	private List<String> script = new ArrayList<String>();
 	
 	
